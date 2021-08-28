0a0bf74bba955be9bd179a06d87e79a63892d4d2
piziamko <stoyanovasibina@gmail.com>
2021-08-28 13:03:44 +0300

    Create urls.py

---
+++ 09. Model Forms/books_app/books_app/books/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from books_app.books.views import create_book, index, update_book
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('create/', create_book, name='create book'),
+    path('update/<int:pk>', update_book, name='update book'),
+
+]
