b6fa9a12bca799b1401ace14818c8964d9bfc538
piziamko <stoyanovasibina@gmail.com>
2022-01-05 17:56:25 +0200

    Delete 09. Model Forms directory

--- 09. Model Forms/books_app/books_app/books/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from books_app.books.models import Book
-
-
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    pass
--- 09. Model Forms/books_app/books_app/books/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from books_app.books.models import Book
-
-
-class BookForm(forms.ModelForm):
-    class Meta:
-        model = Book
-        fields = '__all__'
