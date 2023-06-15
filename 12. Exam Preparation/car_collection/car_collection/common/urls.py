77338dc9cfe94c6e6cbbb96d95cb6ad223c5a1b2
Sibina Stoyanova <stoyanovasibina@gmail.com>
2023-06-15 09:37:38 +0300

    Delete 12. Exam Preparation/car_collection directory

--- 12. Exam Preparation/car_collection/car_collection/common/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from car_collection.common.views import home_page
-
-urlpatterns = [
-    path('', home_page, name='home')
-]
--- 12. Exam Preparation/car_collection/car_collection/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('car_collection.common.urls')),
-    path('profile/', include('car_collection.auth_app.urls')),
-    path('car/', include('car_collection.cars_app.urls')),
-]
